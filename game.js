65f68bf7d84a8c2c2c1708ddb55a88b1327eef2f
Froggy_GH <overdmorgan@gmail.com>
2026-09-16 20:38:47 +0930

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
+// Spellbound Legends
+// Main game script
 
+const playButton = document.getElementById("play-button");
+const newWizardButton = document.getElementById("new-wizard-button");
+
+// PLAY
+playButton.addEventListener("click", () => {
+    alert("The adventure is coming soon!");
+});
+
+// NEW WIZARD
+newWizardButton.addEventListener("click", () => {
+    alert("Wizard creation is coming soon!");
+});
